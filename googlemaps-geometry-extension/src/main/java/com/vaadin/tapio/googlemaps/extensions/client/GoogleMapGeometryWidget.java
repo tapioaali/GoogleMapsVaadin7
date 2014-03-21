a45810ff8e6a65ddb120f8e7a5fa722540acb77a
Tapio Aali <tapio@vaadin.com>
2014-03-21 07:41:51 +0200

    Added base stub project for Geometry API extension

---
+++ googlemaps-geometry-extension/src/main/java/com/vaadin/tapio/googlemaps/extensions/client/GoogleMapGeometryWidget.java
@@ -0,0 +1,12 @@
+package com.vaadin.tapio.googlemaps.extensions.client;
+
+import com.google.maps.gwt.client.GoogleMap;
+
+public class GoogleMapGeometryWidget {
+    private GoogleMap map;
+
+    public void setMap(GoogleMap map) {
+        this.map = map;
+    }
+
+}
